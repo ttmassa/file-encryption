006698233d4c9b6a08d8617d14ccae75698bb9a9
Massa Timothée <timothee.massa.perso@gmail.com>
2024-02-14 15:45:57 +0100

    deleted useless storeKey function

--- src/db_manager.c
+++ src/db_manager.c
@@ -25,31 +25,6 @@ void storeFile(const char* fileName, const char* key) {
     sqlite3_close(db);
 }
 
-void storeKey(char* key) {
-    sqlite3 *db;
-    char *err_msg = 0;
-
-    int rc  = sqlite3_open("C:/Users/timot/Documents/C/file-encryption/test.db", &db);
-
-    if (rc != SQLITE_OK) {
-        fprintf(stderr, "Cannot open database: %s\n", sqlite3_errmsg(db));
-        sqlite3_close(db);
-    }
-
-    char sql[256];
-    sprintf(sql, "INSERT INTO KEYS(Filepath) VALUES('%s')", key);
-
-    rc = sqlite3_exec(db, sql, 0, 0, &err_msg);
-
-    if (rc != SQLITE_OK) {
-        fprintf(stderr, "SQL error: %s\n", err_msg);
-        sqlite3_free(err_msg);
-        sqlite3_close(db);
-    }
-
-    sqlite3_close(db);
-}
-
 void initializeDb() {
     sqlite3 *db;
     char *err_msg = 0;
