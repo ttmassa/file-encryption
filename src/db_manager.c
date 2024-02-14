5f6a2121f10e368ad1fb6a91136f01c7fab5a299
Massa Timothée <timothee.massa.perso@gmail.com>
2024-02-14 15:55:38 +0100

    getKey function added

--- src/db_manager.c
+++ src/db_manager.c
@@ -1,6 +1,12 @@
 #include <sqlite3.h>
 #include <stdio.h>
 
+static int callback(void *key, int argc, char **argv, char **azColName){
+   char** result = (char**)key;
+   *result = strdup(argv[0]);
+   return 0;
+}
+
 void storeFile(const char* fileName, const char* key) {
     sqlite3 *db;
     char *err_msg = 0;
@@ -13,7 +19,7 @@ void storeFile(const char* fileName, const char* key) {
     }
 
     char sql[256];
-    sprintf(sql, "INSERT INTO KEYS(Filepath, Key) VALUES('%s', '%s')", fileName, key);
+    sprintf(sql, "INSERT INTO KEYS(Name, Key) VALUES('%s', '%s')", fileName, key);
     rc = sqlite3_exec(db, sql, 0, 0, &err_msg);
 
     if (rc != SQLITE_OK) {
@@ -23,6 +29,35 @@ void storeFile(const char* fileName, const char* key) {
     }
 
     sqlite3_close(db);
+}
+
+char* getKey(char* fileName) {
+    sqlite3 *db;
+    char *err_msg = 0;
+    char* key = NULL;
+
+    int rc  = sqlite3_open("C:/Users/timot/Documents/C/file-encryption/test.db", &db);
+
+    if (rc != SQLITE_OK) {
+        fprintf(stderr, "Cannot open database: %s\n", sqlite3_errmsg(db));
+        sqlite3_close(db);
+        return NULL;
+    }
+
+    char sql[256];
+    sprintf(sql, "SELECT Key FROM KEYS WHERE Name = '%s'", fileName);
+    rc = sqlite3_exec(db, sql, callback, &key, &err_msg);
+
+    if (rc != SQLITE_OK) {
+        fprintf(stderr, "SQL error: %s\n", err_msg);
+        sqlite3_free(err_msg);
+        sqlite3_close(db);
+        return NULL;
+    }
+
+    sqlite3_close(db);
+
+    return key;
 }
 
 void initializeDb() {
@@ -37,7 +72,7 @@ void initializeDb() {
         return;
     }
 
-    char *sql = "CREATE TABLE IF NOT EXISTS KEYS(Id INTEGER PRIMARY KEY, Filepath TEXT UNIQUE,Key TEXT);";
+    char *sql = "CREATE TABLE IF NOT EXISTS KEYS(Id INTEGER PRIMARY KEY, Name TEXT UNIQUE,Key TEXT);";
 
     rc = sqlite3_exec(db, sql, 0, 0, &err_msg);
 
